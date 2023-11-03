0151ec6a08a70d7342d2a95be4b15407e5bbebce
Tanmay Phalke <tanyphalke1210@gmail.com>
2023-11-03 22:28:56 +0530

    1. Registration Not Working 2. Login Not Working 3. MainActivity to be fixed to goto OnBoardingActivity after successfull login.

--- app/src/main/java/com/example/sajnistore/OnBoardingActivity.java
+++ app/src/main/java/com/example/sajnistore/OnBoardingActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
@@ -13,18 +15,40 @@ public class OnBoardingActivity extends AppCompatActivity {
     LinearLayout dotsLayout;
     WelcomeSliderAdapter welcomeSliderAdapter;
 
+    TextView[] dots;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.F);
+        //hide status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //hide toolbar
+        getSupportActionBar().hide();
 
         setContentView(R.layout.activity_on_boarding);
 
         viewPager = findViewById(R.id.slider);
         dotsLayout = findViewById(R.id.dots);
 
+        addDots(0);
+
         //Calling the adapter
         welcomeSliderAdapter = new WelcomeSliderAdapter(this);
         viewPager.setAdapter(welcomeSliderAdapter);
+    }
+
+    private void addDots(int position){
+        dots = new TextView[3];
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i]=new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if(dots.length>0){
+            dots[position].setTextColor(Integer.parseInt("#EC407A"));
+        }
     }
 }
